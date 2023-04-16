683457df82680258d39a21741b9c7af56d564e46
Liubov <lrlr2393@gmail.com>
2023-04-16 12:34:44 +0500

    List, detail view, blueprint, bootstrap, templates

--- blog/app.py
+++ blog/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from .views.users import users_app
+from .views.articles import articles_app
 
 app = Flask(__name__)
 
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix='/articles')
+
 @app.route("/")
 def index():
-    return "Hello web!"
+    return render_template('index.html')
 
-# @app.route("/<name>/")
-# def index(name:str):
-#     return f"Hello {name}!"
