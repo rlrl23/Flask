a86af8dffa16f16dc07f8278a5a5f13848a8725f
Liubov <lrlr2393@gmail.com>
2023-04-20 11:31:54 +0500

    SQlAlchemy, flask_login, session authentification

--- blog/views/users.py
+++ blog/views/users.py
@@ -1,22 +1,20 @@
 from flask import Blueprint, render_template
 from werkzeug.exceptions import NotFound
+from ..models.user import User
+
 
 users_app = Blueprint("users_app", __name__)
-USERS = {
-1: "James",
-2: "Brian",
-3: "Peter",
-}
 
-@users_app.route("/", endpoint='list')
+@users_app.route("/users/", endpoint='list')
 def users_list():
-    return render_template("users/list.html", users=USERS)
+    users=User.query.all()
+    return render_template("users/list.html", users=users)
 
-@users_app.route("/<int:user_id>/", endpoint='details')
+@users_app.route("/users/<int:user_id>/", endpoint='details')
 def user_details(user_id:int):
-    try:
-        user_name=USERS[user_id]
-    except KeyError:
+    user=User.query.filter_by(id=user_id).one_or_none()
+    print(user)
+    if user is None:
         raise NotFound(f'User #{user_id} doesn`t exist!')
 
-    return render_template("users/details.html", user_id=user_id, user_name=user_name)
+    return render_template("users/details.html", user=user)
