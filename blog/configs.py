49ce698a98e6de5649665aebafc7b4eb0162aa0b
rlrl23 <rlrl23@mail.ru>
2023-06-06 20:24:18 +0500

    config

--- blog/configs.py
+++ blog/configs.py
@@ -2,12 +2,14 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+BASE_DIR = os.path.abspath(path='')
+# .dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = os.getenv('DEBUG')
 #TESTING = os.getenv('TESTING')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/vibbr/PycharmProjects/Flask/blog/db.sqlite'
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{BASE_DIR}/blog/db.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '12345678l'
 WTF_CSRF_ENABLED = True
 FLASK_ADMIN_SWATCH = 'cosmo'
 OPENAPI_URL_PREFIX = '/api/swagger'
@@ -17,4 +19,5 @@ OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
 PYTHON_VERSION= '3.11'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR, 'yes')
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
